2ef315a7939dfe63c3723426c68ec94ac815af70
Missael Sanchez <missaelsanz.14@outlook.com>
2025-03-10 23:32:25 -0600

    Refactor app header and improve scatter plot button functionality

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header('Precio de autos')
-
-st.write('Esta app esta en proceso de construccion')
+st.header('Analisis de precios de autos')
 
 car_data = pd.read_csv('/Users/missa/LEARNING_PYTHON_LOCAL/PROYECT_SPRINT7/vehicles_us.csv')
 hist_button = st.button('Construir histograma') #crear un boton
@@ -19,8 +17,8 @@ if hist_button: #si presiona el boton
 
 st.write('Grafico de dispersion')
 #crear scatter plot
-if st.button('Construit grafico de dispersion')
-dispersion = px.scatter(car_data, x='model_year', y='price', title='Precio vs Año')
-st.plotly_chart(dispersion, use_container_width=True)
+if st.button('Construit grafico de dispersion'):
+    dispersion = px.scatter(car_data, x='model_year', y='price', title='Precio vs Año')
+    st.plotly_chart(dispersion, use_container_width=True)
 
 
