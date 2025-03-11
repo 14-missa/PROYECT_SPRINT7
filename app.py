b1ee6b25f5e5a8119d076568f58017974763b02c
Missael Sanchez <missaelsanz.14@outlook.com>
2025-03-10 23:26:22 -0600

    Add header and initial content to car pricing app; implement scatter plot functionality

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.header('Precio de autos')
+
+st.write('Esta app esta en proceso de construccion')
+
 car_data = pd.read_csv('/Users/missa/LEARNING_PYTHON_LOCAL/PROYECT_SPRINT7/vehicles_us.csv')
 hist_button = st.button('Construir histograma') #crear un boton
 
@@ -15,9 +19,8 @@ if hist_button: #si presiona el boton
 
 st.write('Grafico de dispersion')
 #crear scatter plot
-dispersion = px.scatter(car_data, x='year', y='price', title='Precio vs Año')
+if st.button('Construit grafico de dispersion')
+dispersion = px.scatter(car_data, x='model_year', y='price', title='Precio vs Año')
 st.plotly_chart(dispersion, use_container_width=True)
 
-st.header('Precio de autos')
 
-st.write('Esta app esta en proceso de construccion')
