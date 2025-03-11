b5201c9db0eb015e7626a686e586ab5157171525
Missael Sanchez <missaelsanz.14@outlook.com>
2025-03-10 23:16:09 -0600

    Add interactive visualizations for car data using Plotly

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('/Users/missa/LEARNING_PYTHON_LOCAL/PROYECT_SPRINT7/vehicles_us.csv')
+hist_button = st.button('Construir histograma') #crear un boton
+
+if hist_button: #si presiona el boton
+    #da un histograma
+    st.write('Histograma de precios')
+    #crear histograma
+    fig = px.histogram(car_data, x="odometer", title='millas recorridas')
+    #Mostrar plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+st.write('Grafico de dispersion')
+#crear scatter plot
+dispersion = px.scatter(car_data, x='year', y='price', title='Precio vs Año')
+st.plotly_chart(dispersion, use_container_width=True)
 
 st.header('Precio de autos')
 
