40ea7c14403ee8c9d01d20d2ed21e8a9f4658550
Missael Sanchez <missaelsanz.14@outlook.com>
2025-04-08 00:36:03 -0600

    cambiar ruta relativa

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.header('Analisis de precios de autos')
 
-car_data = pd.read_csv('/Users/missa/LEARNING_PYTHON_LOCAL/PROYECT_SPRINT7/vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma') #crear boton histograma
 scatter_button = st.button('Construir grafico de dispersion') #crear boton scatter
 if hist_button: #si presiona el boton
