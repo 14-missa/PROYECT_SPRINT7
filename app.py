895376d95fe17770cbdf6c8c886857ca1b2306b7
Missael Sanchez <missaelsanz.14@outlook.com>
2025-04-07 23:39:01 -0600

    Corrigiendo para entrega

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import plotly.express as px
 st.header('Analisis de precios de autos')
 
 car_data = pd.read_csv('/Users/missa/LEARNING_PYTHON_LOCAL/PROYECT_SPRINT7/vehicles_us.csv')
-hist_button = st.button('Construir histograma') #crear un boton
-
+hist_button = st.button('Construir histograma') #crear boton histograma
+scatter_button = st.button('Construir grafico de dispersion') #crear boton scatter
 if hist_button: #si presiona el boton
     #da un histograma
     st.write('Histograma de precios')
@@ -15,9 +15,10 @@ if hist_button: #si presiona el boton
     #Mostrar plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-st.write('Grafico de dispersion')
+
 #crear scatter plot
-if st.button('Construit grafico de dispersion'):
+if scatter_button:
+    st.write('Grafico de dispersion')
     dispersion = px.scatter(car_data, x='model_year', y='price', title='Precio vs Año')
     st.plotly_chart(dispersion, use_container_width=True)
 
